eb1db671cbbee83b48ad8c6d6542dfda3ec64500
akhilsai0099 <akhilsai0099@gmail.com>
2023-05-15 01:27:08 +0530

    helper function to fetch the posts

---
+++ src/helper/fetchPosts.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+
+export interface Post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+
+
+// Define the API URL
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+// Function to fetch posts
+export async function fetchPosts(): Promise<Post[]> {
+    try {
+        // Make the API request
+        const response = await axios.get(API_URL);
+
+        // Extract the data from the response
+        const data = response.data;
+
+        // Convert the data into an array of Post objects
+        const posts: Post[] = data.map((post: any) => ({
+            userId: post.userId,
+            id: post.id,
+            title: post.title,
+            body: post.body,
+        }));
+
+        return posts;
+    } catch (error) {
+        // Handle any errors
+        console.error('Error fetching posts:', error);
+        return [];
+    }
+}
